2104be244eec0602fc3b1bc5c556a08090ccd248
GithubUdy <dwt5224@naver.com>
2026-01-12 07:09:39 +0900

    fix : auth.middleware.ts

--- src/photos/photo.routes.ts
+++ src/photos/photo.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { uploadPhoto } from './photo.controller'
-import authenticate from '../auth/auth.middleware'
+import { authenticate } from '../auth/auth.middleware'
 import { upload } from '../config/multer'
 
 const router = Router()
