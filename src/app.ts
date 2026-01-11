c76378755c26049f7922846f825336942304c098
GithubUdy <dwt5224@naver.com>
2026-01-11 17:48:04 +0900

    fix : app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,12 @@ import authRoutes from './auth/auth.routes';
 
 
 const app = express()
-app.use(express.json())
 app.use(routes)
 app.use('/auth', authRoutes)
 app.get('/', (req, res) => {
   res.json({status: 'ok'})
 })
 app.use(cosmeticRoutes);
+app.use(express.json())
 
 export default app
