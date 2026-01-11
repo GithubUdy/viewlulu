120cccaa6434a5e4c7bfc1648f748cb52062af7e
GithubUdy <dwt5224@naver.com>
2026-01-11 18:38:39 +0900

    fix server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,6 @@ import app from "./app";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(3000, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
